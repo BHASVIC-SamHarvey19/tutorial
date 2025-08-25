3c17a0e08aa55ba9061ab7c5e36407f95c4e90d0
BHASVIC-SamHarvey19 <sam.harvey24@students.bhasvic.ac.uk>
2025-08-25 18:01:13 +0100

    completed javadoc from command line tutorial

---
+++ PointTester.java
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * PointTester tests the point class in Point.java. This is a second sentence.
+ *
+ * @author samoh
+ * @version 1.0
+ * @see Point
+ *
+ *
+ *
+ *
+ */
+public class PointTester {
+
+    /**
+     * this is the main() method for the driver program
+     * @param args stores the incoming command line arguments for the program
+     */
+    public static void main(String[] args) {
+        Point origin = new Point();
+        System.out.println(origin);
+    }
+}
